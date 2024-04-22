dda2631369523496856d502739b2073725e7c12b
yeeun <84173008+jye104@users.noreply.github.com>
2024-04-22 15:40:16 +0900

    Form에 이벤트 등록해주기

--- src/components/CalculatorForm.jsx
+++ src/components/CalculatorForm.jsx
@@ -1,14 +1,39 @@
+import { useState } from 'react';
+
 const CalculatorForm = () => {
+  const [inputState, setInputState] = useState({});
+
+  const handleInlutChanges = (e) => {
+    setInputState({
+      ...inputState,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleReset = () => {};
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); //submit 버튼을 눌렀을때 페이지가 리로드 되는 것을 멈추기
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={handleSubmit}>
       <div className="input-group">
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
-          <input type="number" id="current-savings" />
+          <input
+            type="number"
+            id="current-savings"
+            onChange={handleInlutChanges}
+          />
         </p>
         <p>
           <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-          <input type="number" id="yearly-contribution" />
+          <input
+            type="number"
+            id="yearly-contribution"
+            onChange={handleInlutChanges}
+          />
         </p>
       </div>
       <div className="input-group">
@@ -16,15 +41,19 @@ const CalculatorForm = () => {
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
           </label>
-          <input type="number" id="expected-return" />
+          <input
+            type="number"
+            id="expected-return"
+            onChange={handleInlutChanges}
+          />
         </p>
         <p>
           <label htmlFor="duration">Investment Duration (years)</label>
-          <input type="number" id="duration" />
+          <input type="number" id="duration" onChange={handleInlutChanges} />
         </p>
       </div>
       <p className="actions">
-        <button type="reset" className="buttonAlt">
+        <button type="reset" className="buttonAlt" onClick={handleReset}>
           Reset
         </button>
         <button type="submit" className="button">
